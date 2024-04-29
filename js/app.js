2472c26502b06b471a08ec5c785009e6c1627406
viyoko0205 <viyoko0205@naver.com>
2024-04-29 12:49:52 +0900

    0429

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param {NodeList} elmts  selected elements from HTML to add events
+ * @param {String} event event type e.g. 'click', 'mouseenter'...
+ * @param {Function} callback callback function to be executed when event is triggered
+ */
+
+export function eventOnElmts(elmts, event, callback) {
+  for (const elmt of elmts) {
+    elmt.addEventListener(event, callback);
+  }
+}
