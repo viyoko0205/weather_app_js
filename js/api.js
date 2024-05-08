33369fcf88a5cc284c171efd5e63ad42daaa2918
viyoko0205 <viyoko0205@naver.com>
2024-05-08 12:39:10 +0900

    finished search function

--- js/api.js
+++ js/api.js
@@ -1,9 +1,9 @@
-const api_key = '85da053fda57d86314a2a898ec5cd015';
+const api_key = '30f79eac8f2abb1b72a3955b3e52147d';
 
-export const urls = {
+export const url = {
   //endpoint urls
   currentWeather(lat, lon) {
-    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=matric`;
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
   },
 
   geocode(loca) {
@@ -11,15 +11,17 @@ export const urls = {
   },
 };
 
-const fetchData = function (url, callback) {
-  fetch(`${url}$appid=${api_key}`)
+/**
+ *
+ * @param {string} url
+ * @param {function} callback
+ */
+
+export const fetchData = function (url, callback) {
+  fetch(`${url}&appid=${api_key}`)
     .then((response) => response.json())
     .then((data) => callback(data))
     .catch((error) => console.log(error));
 };
-
-fetchData(urls.geocode('london'), function (locations) {
-  console.log(locations);
-});
 
 //구조분해 할당
